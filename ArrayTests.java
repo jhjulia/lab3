1664bc70abb091ad3eba725c02791b19e04a519a
jhjulia <jibang@ucsd.edu>
2022-10-14 16:11:32 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,26 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlace_2(){
+    int[] input1 = {0, 3, 5, 7, 11, 5};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{5, 11, 7, 5, 3, 0}, input1);
+  }
+
+  @Test
+  public void testReversed_2(){
+    int[] input1 = {1, 2, 3, 4, 5, 6};
+    assertArrayEquals(new int[]{6, 5, 4, 3, 2, 1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testAverage(){
+    double[] input1 = {6, 4, 2, 8, 4};
+    assertEquals(11, ArrayExamples.averageWithoutLowest(input1));
+  }
 }
+
+
+
